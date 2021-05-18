03e6b744b7b745b7341a5e97381290efe74fde29
Kenneth DAquila <kfd182@gmail.com>
2021-05-18 02:05:39 -0500

    Added workout sort icons buttons

--- routes/browse.js
+++ routes/browse.js
@@ -5,8 +5,10 @@ const printf = require('printf');
 
 router.get('/', async function(req, res, next) {
   try {
+    res.locals.sortKey = req.query.sortKey || "CreationDate";
+    res.locals.sortDir =req.query.sortDir || "-1";
     let pageNumber = Number(req.query.page) || 0;
-    let workouts = await findWorkouts(res.locals.user.Username, pageNumber);
+    let workouts = await findWorkouts(res.locals.user.Username, pageNumber, res.locals.sortKey, Number(res.locals.sortDir));
     res.locals.workoutCount = await countWorkouts(res.locals.user.Username);
     res.locals.workouts = workouts;
     res.locals.printf = printf;
@@ -16,6 +18,13 @@ router.get('/', async function(req, res, next) {
     res.locals.isNextAvail = workouts && workouts.length > 0 && res.locals.stopIndex < res.locals.workoutCount;
     res.locals.nextPage = pageNumber + 1;
     res.locals.prevPage = ((pageNumber - 1) < 0) ? 0 : (pageNumber - 1);
+
+    res.locals.query = Object.entries(req.query).reduce((accumulator, [key,value])=>{
+      if (key === "sortKey" || key === "sortDir") {
+        accumulator = accumulator + key + "=" + value + "&";
+      }
+      return accumulator;
+    }, "")
     res.render('browse');
   }
   catch (error) {
@@ -36,7 +45,7 @@ router.post('/delete', async function(req, res, next) {
 
 
 // Database operations for these routes
-async function findWorkouts(Username, pageNumber=0) {
+async function findWorkouts(Username, pageNumber=0, sortKey="CreationDate", sortDir=-1) {
   const client = new MongoClient(process.env.DB_CONNECTION_STRING, { useUnifiedTopology: true });
   try {
     await client.connect();
@@ -79,9 +88,11 @@ async function findWorkouts(Username, pageNumber=0) {
       }},
 
     ]
+    let sortObj = {}
+    sortObj[sortKey] = sortDir;
     let workouts = await db.collection('AppUsers')
       .aggregate(pipeline)
-      .sort({'CreationDate': -1})
+      .sort(sortObj)
       .skip(pageNumber*Number(process.env.WORKOUTS_PER_PAGE))
       .limit(Number(process.env.WORKOUTS_PER_PAGE))
     workouts = await workouts.toArray();
